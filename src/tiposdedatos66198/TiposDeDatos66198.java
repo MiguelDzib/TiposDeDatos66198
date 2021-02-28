16087cc9ad44bab998749c1211d8521f6b5f50d4
galbe <galbe@192.168.1.68>
2021-02-28 09:36:28 -0600

    Tipos de datos 66198

---
+++ src/tiposdedatos66198/TiposDeDatos66198.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatos66198;
+
+public class TiposDeDatos66198 {
+
+    public static void main(String[] args) {
+       byte byteMax= Byte.MAX_VALUE ;
+         System.out.println(" El valor maximo de byte es=" +  byteMax  ) ;
+         
+    byte byteMin= Byte.MIN_VALUE ;
+         System.out.println(" El valor minimo de byte es=" +  byteMin ) ;
+      
+    short shortMax= Short.MAX_VALUE  ;
+         System.out.println(" El valor maximo de short es=" +  shortMax  ) ;
+    
+    short shortMin= Short.MIN_VALUE  ;
+         System.out.println(" El valor minimo de short es=" +  shortMin  ) ;
+         
+    int intengerMax= Integer.MAX_VALUE;
+        System.out.println(" El valor maximo de lnteger es=" +  intengerMax ) ;
+        
+    int intengerMin= Integer.MIN_VALUE;
+        System.out.println(" El valor minimo de lnteger es=" +  intengerMin ) ;
+                  
+    long longMax= Long.MAX_VALUE ;
+         System.out.println(" El valor maximo de long es=" +  longMax  ) ;
+         
+    long longMin= Long.MIN_VALUE  ;
+         System.out.println(" El valor minimo de long es=" +  longMin   ) ;
+    
+    double doubleMax= Double.MAX_VALUE ;
+           System.out.println(" El valor maximo de double es=" +  doubleMax  );
+      
+    double doubleMin= Double.MIN_VALUE ;
+           System.out.println(" El valor minimo de double es=" +  doubleMin   ); 
+        
+        
+        
+    }
+    
+}
